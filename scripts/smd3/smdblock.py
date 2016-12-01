0a14bb38c5eda87ccbffcae5f6f625230d7b2794
p-hofmann <peter.hofmann@hhu.de>
2016-12-01 15:32:27 +0100

    Better docstring

--- scripts/smd3/smdblock.py
+++ scripts/smd3/smdblock.py
@@ -19,19 +19,42 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 		self._byte_string = ""
 
 	def get_id(self):
+		"""
+		Returns the block id
+
+		@rtype: int
+		"""
 		return self._id
 
 	def get_hitpoints(self):
+		"""
+		Returns the hit points of the block
+
+		@rtype: int
+		"""
 		return self._hitpoints
 
 	def is_active(self):
+		"""
+		Returns the 'active' status.
+
+		@rtype: int
+		"""
+		# Todo: No idea what this is, logic maybe?
 		return self._active == 1
 
 	def get_orientation(self):
+		"""
+		Returns the orientation of the block as integer.
+
+		@rtype: int
+		"""
+		# Todo: find out what integer is what direction.
 		return self._orientation
 
 	def set_id(self, value):
 		"""
+		Change block id of block
 
 		@param value:
 		@type value: int
@@ -41,6 +64,7 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def set_hitpoints(self, value):
 		"""
+		Change hit points of block
 
 		@param value:
 		@type value: int
@@ -50,6 +74,7 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def set_active(self, value):
 		"""
+		Change 'active' status of of block
 
 		@param value:
 		@type value: bool
@@ -62,6 +87,7 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def set_orientation(self, value):
 		"""
+		Change orientation of block
 
 		@param value:
 		@type value: int
@@ -71,6 +97,8 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def set_data_byte_string(self, byte_string):
 		"""
+		Change the byte string representing the block and parse it for easy access.
+		The byte string is kept to speed up writing files.
 
 		@param byte_string:
 		@type byte_string: str
@@ -80,12 +108,18 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def get_data_byte_string(self):
 		"""
+		Returns the byte string representing a block.
 
 		@rtype: str
 		"""
 		return self._byte_string
 
 	def _parse_byte_string(self):
+		"""
+		The byte string is turned into an integer so bit operations can pick out each value.
+		An integer is 4 byte (32 bit) but a block is only 3 byte long. THis is why '\x00' is added.
+		'\x00' is added first because of big endian.
+		"""
 		# bit_array = self._read_int24_unassigned(input_stream)
 		bit_array = struct.unpack('>i', '\x00' + self._byte_string)[0]
 		self._id = self.parse_bits(bit_array, 0, 11)
@@ -94,6 +128,10 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 		self._orientation = self.parse_bits(bit_array, 21, 3)
 
 	def _refresh_data_byte_string(self):
+		"""
+		In the rare case a block value is changed, they are turned into a byte string.
+		'[1:]' since only the last three bytes of an integer are used for block information.
+		"""
 		bit_array = 0
 		bit_array = self.combine_bits(self._id, bit_array, 0)
 		bit_array = self.combine_bits(self._hitpoints, bit_array, 11)
@@ -103,6 +141,7 @@ class SmdBlock(BitAndBytes, BlueprintUtils):
 
 	def to_stream(self, output_stream=sys.stdout):
 		"""
+		Stream block values
 
 		@param output_stream:
 		@type output_stream: fileIO
