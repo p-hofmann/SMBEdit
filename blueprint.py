da8d37a104e6b7a485bbc71247d793ab7ba11b9f
p-hofmann <peter.hofmann@hhu.de>
2016-12-03 23:35:28 +0100

    added docstring

--- blueprint.py
+++ blueprint.py
@@ -1,19 +1,31 @@
 __author__ = 'Peter Hofmann'
 
 import os
+import sys
 from scripts.loggingwrapper import DefaultLogging
 from scripts.blueprintutils import BlueprintUtils
 from scripts.header import Header
 from scripts.logic import Logic
 from scripts.meta import Meta
-from scripts.smd import Smd
+from scripts.smd3.smd import Smd
 
 
 class Blueprint(DefaultLogging, BlueprintUtils):
 
-	_label = "Blueprint"
-
 	def __init__(self, logfile=None, verbose=False, debug=False):
+		"""
+		Constructor
+
+		@param logfile: file handler or file path to a log file
+		@type logfile: file | FileIO | StringIO | basestring
+		@param verbose: Not verbose means that only warnings and errors will be past to stream
+		@type verbose: bool
+		@param debug: Display debug messages
+		@type debug: bool
+
+		@rtype: None
+		"""
+		self._label = "Blueprint"
 		super(Blueprint, self).__init__(
 			logfile=logfile,
 			verbose=verbose,
@@ -29,16 +41,20 @@ class Blueprint(DefaultLogging, BlueprintUtils):
 	# #######################################
 
 	def read(self, directory_blueprint):
+		"""
+		Read blueprint from a directory
+
+		@param directory_blueprint: /../StarMade/blueprints/blueprint_name/
+		@type directory_blueprint: str
+		"""
 		self.header = Header()
-		self.header.read(directory_blueprint)
 		self.logic = Logic()
-		self.logic.read(directory_blueprint)
 		self.meta = Meta()
-		# self.meta.read(directory_blueprint)
-		self.smd3 = Smd(
-			logfile=self._logfile,
-			verbose=self._verbose,
-			debug=self._debug)
+		self.smd3 = Smd(logfile=self._logfile, verbose=self._verbose, debug=self._debug)
+
+		self.header.read(directory_blueprint)
+		self.logic.read(directory_blueprint)
+		self.meta.read(directory_blueprint)
 		self.smd3.read(directory_blueprint)
 
 	# #######################################
@@ -46,15 +62,20 @@ class Blueprint(DefaultLogging, BlueprintUtils):
 	# #######################################
 
 	def write(self, directory_blueprint):
+		"""
+		Save blueprint to a directory
+
+		@param directory_blueprint: /../StarMade/blueprints/blueprint_name/
+		@type directory_blueprint: str
+		"""
 		if directory_blueprint.endswith('/') or directory_blueprint.endswith('\\'):
 			directory_blueprint = directory_blueprint[:-1]
-		# root_dir = os.path.dirname(directory_blueprint)
+
 		blueprint_name = os.path.basename(directory_blueprint)
 		directory_blueprint_unique = directory_blueprint
 		assert not os.path.exists(directory_blueprint_unique), "Output folder exists, aborting."
 		os.mkdir(directory_blueprint_unique)
-		# directory_blueprint_unique = tempfile.mkdtemp(prefix=blueprint_name+"_", dir=root_dir)
-		# print "Output:", directory_blueprint_unique
+
 		self.header.write(directory_blueprint_unique)
 		self.logic.write(directory_blueprint_unique)
 		self.meta.write(directory_blueprint_unique)
@@ -64,10 +85,13 @@ class Blueprint(DefaultLogging, BlueprintUtils):
 	# ###  Else
 	# #######################################
 
-	def _get_direction_vector_to_center(self, position):
-		return self.vector_subtraction(position, (16, 16, 16))
-
 	def move_center_by_block_id(self, block_id):
+		"""
+		Relocate center/core to the position of a hopefully unique block
+
+		@param block_id: block id
+		@type block_id: int
+		"""
 		assert isinstance(block_id, (int, long))
 		position = self.smd3.search(block_id)
 		assert position is not None, "Block id not found: {}".format(block_id)
@@ -78,28 +102,50 @@ class Blueprint(DefaultLogging, BlueprintUtils):
 		self.move_center_by_vector(direction_vector)
 
 	def set_entity_type(self, entity_type):
+		"""
+		Change entity type
+		0: "Ship",
+		2: "Station",
+
+		@param entity_type: ship=0/station=2/etc
+		@type entity_type: int
+		"""
 		assert isinstance(entity_type, (int, long))
 		self.header.set_type(entity_type)
 		self.logic.set_type(entity_type)
 		self.smd3.set_type(entity_type)
 
 	def update(self):
+		"""
+		Remove invalid/outdated blocks and exchange docking modules with rails
+		"""
 		entity_type = self.header.type
-		# self.header.update()
 		self.logic.update()
 		self.smd3.update(entity_type)
-		min_vector, max_vector = self.smd3.get_min_max_vector()
-		self.header.set_box(min_vector, max_vector)
-		self.header.set_quantities(self.smd3.get_block_id_to_quantity())
+		self.header.update(self.smd3)
 
 	def move_center_by_vector(self, direction_vector):
+		"""
+		Relocate center/core in a direction
+
+		@param direction_vector: vector
+		@type direction_vector: int, int, int
+		"""
 		assert isinstance(direction_vector, tuple)
 		min_vector, max_vector = self.smd3.move_center(direction_vector)
 		self.logic.move_center(direction_vector, self.header.type)
 		self.header.set_box(min_vector, max_vector)
 
-	def to_string(self, summary=True):
-		self.header.to_stream(summary=summary)
-		self.logic.to_stream(summary=summary)
-		# self.meta.to_stream()
-		self.smd3.to_stream(summary=summary)
+	def to_stream(self, output_stream=sys.stdout, summary=True):
+		"""
+		Stream blueprint values
+
+		@param output_stream: Output stream
+		@type output_stream: fileIO
+		@param summary: If true the output is reduced
+		@type summary: bool
+		"""
+		self.header.to_stream(output_stream, summary=summary)
+		self.logic.to_stream(output_stream, summary=summary)
+		# self.meta.to_stream(output_stream, summary=summary)
+		self.smd3.to_stream(output_stream, summary=summary)
