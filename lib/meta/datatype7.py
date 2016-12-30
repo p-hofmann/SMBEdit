e9f96d9ac33e720d4b0cfff735d1a99463ca9fb1
p-hofmann <peter.hofmann@hhu.de>
2016-12-30 14:34:36 +0100

    fixed data types

--- lib/meta/datatype7.py
+++ lib/meta/datatype7.py
@@ -10,7 +10,7 @@ class DataType7(DefaultLogging):
 	def __init__(self, logfile=None, verbose=False, debug=False):
 		self._label = "Meta-DataType6"
 		super(DataType7, self).__init__(logfile, verbose, debug)
-		self._root_tag = 1
+		self._has_data = 1
 		self._data = {}
 		return
 
@@ -20,26 +20,22 @@ class DataType7(DefaultLogging):
 
 	def read(self, input_stream):
 		"""
-		Read tag root from byte stream
-
-		# 07 01		00 00 00 00
-		# 07 01 	00 00 00 02
-		# 											00 00 ff fe  00 0e 00 04  40 59 00 00  00 00 00 00
-		# 											00 00 ff fe  00 0e 00 1c  40 59 00 00  00 00 00 00
+		Read from byte stream
 
 		@param input_stream: input stream
 		@type input_stream: ByteStream
 		"""
-		self._root_tag = input_stream.read_byte()
-		if self._root_tag == 1:
-			unknown_int32 = input_stream.read_int32_unassigned()
-			assert unknown_int32 < 10000, unknown_int32
+		self._has_data = input_stream.read_byte()
+		if self._has_data > 0:
+			number_of_entries = input_stream.read_int32_unassigned()
+			assert number_of_entries < 10000, number_of_entries
 			self._data = {}
-			for index in range(unknown_int32):
-				# self._data[index] = input_stream.read_vector_x_int32(4)
-				self._data[index] = input_stream.read_vector_4_float()
+			for _ in range(number_of_entries):
+				key = input_stream.read_int64()
+				value = input_stream.read_double()
+				self._data[key] = value
 		else:
-			raise NotImplementedError("Unknown tag: {}".format(self._root_tag))
+			raise NotImplementedError("Unknown tag: {}".format(self._has_data))
 
 	# #######################################
 	# ###  Write
@@ -56,10 +52,13 @@ class DataType7(DefaultLogging):
 		# 	return
 		self._logger.debug("Writing")
 		output_stream.write_byte(7)
-		output_stream.write_byte(self._root_tag)
+		output_stream.write_byte(self._has_data)
+		if not self._has_data > 0:
+			return
 		output_stream.write_int32_unassigned(len(self._data))
-		for index in sorted(self._data.keys()):
-			output_stream.write_vector_4_float(self._data[index])
+		for key in sorted(self._data.keys()):
+			output_stream.write_int64(key)
+			output_stream.write_double(self._data[key])
 
 	# #######################################
 	# ###  Else
@@ -73,5 +72,5 @@ class DataType7(DefaultLogging):
 		@type output_stream: fileIO
 		"""
 		if self._debug:
-			output_stream.write("DataType7: #{}\n".format(self._data))
+			output_stream.write("DataType7: {}\n".format(self._data))
 			output_stream.write("\n")
