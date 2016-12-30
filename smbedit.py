b66e9a2851af1365812128c2363f0d531b74890f
p-hofmann <peter.hofmann@hhu.de>
2016-12-30 03:18:26 +0100

    added replace_hull and handling of docked entities in most cases

--- smbedit.py
+++ smbedit.py
@@ -43,6 +43,14 @@ class SMBEdit(DefaultLogging):
 	# ###  Read
 	# #######################################
 
+	_char_to_hull_type = {
+		'h': 0,
+		's': 1,
+		'a': 2,
+		'c': 3,
+		'z': 4,
+	}
+
 	@staticmethod
 	def get_parser_options(args=None, version="Prototype"):
 		"""
@@ -142,6 +150,12 @@ class SMBEdit(DefaultLogging):
 			help="old_id,new_id:hit_points")
 
 		group_input.add_argument(
+			"-rh", "--replace_hull",
+			default=None,
+			type=str,
+			help="{0,1,2,3,4}:{0,1,2,3,4}")
+
+		group_input.add_argument(
 			"-o", "--directory_output",
 			default=None,
 			type=str,
@@ -158,93 +172,150 @@ class SMBEdit(DefaultLogging):
 		else:
 			return parser.parse_args(args)
 
+	@staticmethod
+	def _clean_dir_path(directory):
+		return directory.rstrip('/').rstrip('\\')
+
 	def run(self, options):
 		try:
-			directory_blueprint = options.directory_blueprint
+
+			directory_input = options.directory_blueprint
 			directory_output = options.directory_output
 			link_salvage = options.link_salvage
 			index_turn_tilt = options.turn
+			replace_hull = options.replace_hull
 			replace = options.replace
 			move_center = options.move_center
 			update = options.update
 			entity_type = options.entity_type
 			summary = options.summary
 
-			assert os.path.exists(directory_blueprint), "Blueprint directory does not exist, aborting."
-			assert directory_output is None or not os.path.exists(directory_output), "Output directory exists, not writing blueprint to exiting folder to prevent overwriting."
-
-			blueprint = Blueprint(
-				logfile=self._logfile,
-				verbose=self._verbose,
-				debug=self._debug,
-			)
-
-			self._logger.info("Reading blueprint...")
-			blueprint.read(directory_blueprint)
-
-			if move_center is not None:
-				self._logger.info("Moving center/core of blueprint...")
-				if ',' in move_center:  # vector
-					direction_vector = [0, 0, 0]
-					tmp = move_center.split(',')
-					assert len(tmp) == 3, "Bad vector: '{}'".format(move_center)
-					for index, value in enumerate(tmp):
-						assert isinstance(value, basestring)
-						# assert value.isdigit(), "Bad vector: '{}'".format(move_center)
-						direction_vector[index] = int(value)
-					blueprint.move_center_by_vector(tuple(direction_vector))
-				else:  # block id
-					assert move_center.isdigit(), "Bad block id: '{}'".format(move_center)
-					blueprint.move_center_by_block_id(int(move_center))
-
-			if replace is not None:
-				self._logger.info("Replacing blocks...")
-				assert isinstance(replace, str)
-				assert ',' in replace, "Bad replace: '{}'".format(replace)
-				old_block_id, suffix = replace.split(',')
-				assert old_block_id.isdigit(), "Bad old block id: '{}'".format(old_block_id)
-				old_block_id = int(old_block_id)
-
-				assert ':' in suffix, "Bad replace: '{}'".format(replace)
-				replace_id, replace_hp = suffix.split(':')
-				assert replace_id.isdigit(), "Bad replace block id: '{}'".format(replace_id)
-				assert replace_hp.isdigit(), "Bad replace block hp: '{}'".format(replace_hp)
-				replace_id = int(replace_id)
-				replace_hp = int(replace_hp)
-				blueprint.replace_blocks(old_block_id, replace_id, replace_hp)
-
-			if index_turn_tilt is not None:
-				blueprint.turn_tilt(index_turn_tilt)
-
-			if entity_type is not None:
-				self._logger.info("Changing entity type and updating blueprint...")
-				blueprint.set_entity_type(entity_type)
-
-			if link_salvage:
-				self._logger.info("Linking salvage computers/modules...")
-				blueprint.link_salvage_modules()
-
-			if update and entity_type is None:
-				self._logger.info("Updating blueprint...")
-				blueprint.update()
-
-			if summary:
-				self._logger.info("Summary of blueprint to stdout")
-				blueprint.to_stream()
-
+			assert isinstance(directory_input, str)
+			directory_input = self._clean_dir_path(directory_input)
 			if directory_output is not None:
-				self._logger.info("Saving blueprint to:\n{}".format(directory_output))
-				blueprint.write(directory_output)
+				directory_output = self._clean_dir_path(directory_output)
+			self.run_commands(directory_input, directory_output, link_salvage, index_turn_tilt, replace_hull, replace, move_center, update, entity_type, summary)
 
 		except (KeyboardInterrupt, SystemExit, Exception, ValueError, RuntimeError) as e:
 			self._logger.debug("\n{}\n".format(traceback.format_exc()))
 			if len(e.args) > 0:
 				self._logger.error(e.args[0])
 			self._logger.info("Aborted")
-		except AssertionError:
+		except AssertionError as e:
+			if len(e.args) > 0:
+				self._logger.error(e.args[0])
 			self._logger.info("Aborted")
 		else:
 			self._logger.info("Finished")
+
+	def run_commands(
+		self, directory_input, directory_output, link_salvage, index_turn_tilt, replace_hull, replace, move_center, update, entity_type, summary, blueprint_path=None):
+		assert os.path.exists(directory_input), "Blueprint directory does not exist, aborting."
+		if directory_output is not None:
+			if os.path.exists(directory_output):
+				if len(os.listdir(directory_output)) > 0:
+					raise Exception("Blueprint found in output directory, aborting to prevent overwriting.")
+			else:
+				os.mkdir(directory_output)
+
+		if blueprint_path is None:
+			blueprint_path = directory_output
+		if move_center is None:
+			# if center position is kept, docker can be preserved
+			list_of_folders = os.listdir(directory_input)
+			for folder_name in list_of_folders:
+				if "ATTACHED_" not in folder_name:
+					continue
+				directory_src = os.path.join(directory_input, folder_name)
+				directory_dst = None
+				if directory_output is not None:
+					directory_dst = os.path.join(directory_output, folder_name)
+				self.run_commands(
+					directory_src, directory_dst,
+					link_salvage, index_turn_tilt, replace_hull, replace, move_center, update, entity_type, summary,
+					blueprint_path=blueprint_path)
+
+		blueprint = Blueprint(
+			logfile=self._logfile,
+			verbose=self._verbose,
+			debug=self._debug,
+		)
+
+		blueprint_name = os.path.basename(directory_input)
+		self._logger.info("Reading blueprint '{}' ...".format(blueprint_name))
+		blueprint.read(directory_input)
+
+		if move_center is not None:
+			self._logger.info("Moving center/core of blueprint...")
+			if ',' in move_center:  # vector
+				direction_vector = [0, 0, 0]
+				tmp = move_center.split(',')
+				assert len(tmp) == 3, "Bad vector: '{}'".format(move_center)
+				for index, value in enumerate(tmp):
+					assert isinstance(value, basestring)
+					# assert value.isdigit(), "Bad vector: '{}'".format(move_center)
+					direction_vector[index] = int(value)
+				blueprint.move_center_by_vector(tuple(direction_vector))
+			else:  # block id
+				assert move_center.isdigit(), "Bad block id: '{}'".format(move_center)
+				blueprint.move_center_by_block_id(int(move_center))
+
+		if replace is not None:
+			self._logger.info("Replacing blocks...")
+			assert isinstance(replace, str)
+			assert ',' in replace, "Bad replace: '{}'".format(replace)
+			old_block_id, suffix = replace.split(',')
+			assert old_block_id.isdigit(), "Bad old block id: '{}'".format(old_block_id)
+			old_block_id = int(old_block_id)
+
+			assert ':' in suffix, "Bad replace: '{}'".format(replace)
+			replace_id, replace_hp = suffix.split(':')
+			assert replace_id.isdigit(), "Bad replace block id: '{}'".format(replace_id)
+			assert replace_hp.isdigit(), "Bad replace block hp: '{}'".format(replace_hp)
+			replace_id = int(replace_id)
+			replace_hp = int(replace_hp)
+			blueprint.replace_blocks(old_block_id, replace_id, replace_hp)
+
+		if replace_hull is not None:
+			self._logger.info("Replacing hull...")
+			assert isinstance(replace_hull, str), "Bad replace hull: '{}'".format(replace_hull)
+			assert ':' in replace_hull, "Bad hull replace: '{}'".format(replace_hull)
+			old_hull_type, new_hull_type = replace_hull.split(':', 1)
+			assert new_hull_type in self._char_to_hull_type, "Bad replace hull type: '{}'".format(new_hull_type)
+			new_hull_type = self._char_to_hull_type[new_hull_type]
+			if old_hull_type != "":
+				assert old_hull_type in self._char_to_hull_type, "Bad hull type: '{}'".format(old_hull_type)
+				old_hull_type = self._char_to_hull_type[old_hull_type]
+			else:
+				old_hull_type = None
+			blueprint.replace_hull(new_hull_type, old_hull_type)
+
+		if index_turn_tilt is not None:
+			blueprint.turn_tilt(index_turn_tilt)
+
+		if entity_type is not None:
+			self._logger.info("Changing entity type and updating blueprint...")
+			blueprint.set_entity_type(entity_type)
+
+		if link_salvage:
+			self._logger.info("Linking salvage computers/modules...")
+			blueprint.link_salvage_modules()
+
+		if update and entity_type is None:
+			self._logger.info("Updating blueprint...")
+			blueprint.update()
+
+		if summary:
+			self._logger.info("Summary of blueprint to stdout")
+			blueprint.to_stream()
+
+		if directory_output is not None:
+			self._logger.info("Saving blueprint to:\n{}".format(directory_output))
+			if blueprint_path is None:
+				blueprint.write(directory_output)
+				return
+			relative_path = os.path.relpath(directory_output, os.path.dirname(blueprint_path))
+			blueprint.write(directory_output, relative_path=relative_path)
 
 
 def main():
