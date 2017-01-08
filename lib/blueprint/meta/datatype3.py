1240b262b223fabde051d4ff98b98e96e66b5f00
p-hofmann <peter.hofmann@hhu.de>
2017-01-08 23:48:11 +0100

    added class DockedEntity and method popitem()

--- lib/blueprint/meta/datatype3.py
+++ lib/blueprint/meta/datatype3.py
@@ -1,15 +1,95 @@
 __author__ = 'Peter Hofmann'
 
+import os
 import sys
 from lib.bits_and_bytes import ByteStream
 from lib.loggingwrapper import DefaultLogging
+from lib.blueprint.blueprintutils import BlueprintUtils
+
+
+class DockedEntity(object):
+	"""
+	Class for a docked entity
+
+	@type position: tuple[int]
+	@type size: tuple[float]
+	@type style: int
+	@type orientation: int
+	"""
+	def __init__(self):
+		self._label = "DockedEntity"
+		self.position = (0, 0, 0)
+		self.size = (.0, .0, .0)
+		self.style = 0
+		self.orientation = 0
+
+	# #######################################
+	# ###  Read
+	# #######################################
+
+	def read(self, input_stream):
+		"""
+
+		@param input_stream:
+		@type input_stream: ByteStream
+
+		@return:
+		@rtype: int
+		"""
+		assert isinstance(input_stream, ByteStream)
+		relative_path = input_stream.read_string()
+		self.position = input_stream.read_vector_3_int32()
+		if BlueprintUtils.offset is not None:
+			# in case of sdm2 to sdm3
+			self.position = BlueprintUtils.vector_addition(self.position, BlueprintUtils.offset)
+		self.size = input_stream.read_vector_3_float()
+		self.style = input_stream.read_int16_unassigned()
+		self.orientation = input_stream.read_byte()
+		_, dock_index = relative_path.rsplit('_', 1)
+		return int(dock_index)
+
+	# #######################################
+	# ###  Write
+	# #######################################
+
+	def write(self, output_stream, dock_index, relative_path):
+		"""
+
+		@param output_stream:
+		@type output_stream: ByteStream
+		@type dock_index: int
+		@type relative_path: str
+		"""
+		assert isinstance(output_stream, ByteStream)
+		new_relative_directory = os.path.join(relative_path, "ATTACHED_{}".format(dock_index))
+		output_stream.write_string(new_relative_directory)
+		output_stream.write_vector_3_int32(self.position)
+		output_stream.write_vector_3_float(self.size)
+		output_stream.write_int16_unassigned(self.style)
+		output_stream.write_byte(self.orientation)
+
+	# #######################################
+	# ###  Else
+	# #######################################
+
+	def to_stream(self, output_stream=sys.stdout):
+		"""
+		Stream values
+
+		@param output_stream: Output stream
+		@type output_stream: fileIO
+		"""
+		output_stream.write("Position: {}\t".format(self.position))
+		output_stream.write("Size: {}\t".format(self.size))
+		output_stream.write("Style: {}\t".format(self.style))
+		output_stream.write("Orientation: {}\n".format(self.orientation))
 
 
 class DataType3(DefaultLogging):
 	"""
 	Reading data type 2 meta data
 
-	@type _docked_entity: dict[str, dict[str, int]]
+	@type _docked_entity: dict[int, DockedEntity]
 	"""
 
 	def __init__(self, logfile=None, verbose=False, debug=False):
@@ -21,25 +101,6 @@ class DataType3(DefaultLogging):
 	# #######################################
 	# ###  Read
 	# #######################################
-
-	@staticmethod
-	def _read_dock_entry(input_stream):
-		"""
-
-		@param input_stream:
-		@type input_stream: ByteStream
-
-		@return:
-		@rtype: str, dict
-		"""
-		assert isinstance(input_stream, ByteStream)
-		data = {}
-		name = input_stream.read_string()
-		data["position"] = input_stream.read_vector_3_int32()
-		data["size"] = input_stream.read_vector_3_float()
-		data["style"] = input_stream.read_int16_unassigned()
-		data["orientation"] = input_stream.read_byte()
-		return name, data
 
 	def read(self, input_stream):
 		"""
@@ -53,31 +114,15 @@ class DataType3(DefaultLogging):
 		num_docked = input_stream.read_int32_unassigned()
 		assert 0 <= num_docked < 1000, num_docked  # debug sanity check
 		for index in range(0, num_docked):
-			name, dock_entry = self._read_dock_entry(input_stream)
+			docked_entry = DockedEntity()
+			dock_index = docked_entry.read(input_stream)
 			# self._logger.debug("relative path: '{}': {}".format(name, dock_entry))
-			assert name not in self._docked_entity
-			self._docked_entity[name] = dock_entry
+			assert dock_index not in self._docked_entity
+			self._docked_entity[dock_index] = docked_entry
 
 	# #######################################
 	# ###  Write
 	# #######################################
-
-	@staticmethod
-	def _write_dock_entry(output_stream, name, data):
-		"""
-
-		@param output_stream:
-		@type output_stream: ByteStream
-
-		@return:
-		@rtype: str, dict
-		"""
-		assert isinstance(output_stream, ByteStream)
-		output_stream.write_string(name)
-		output_stream.write_vector_3_int32(data["position"])
-		output_stream.write_vector_3_float(data["size"])
-		output_stream.write_int16_unassigned(data["style"])
-		output_stream.write_byte(data["orientation"])
 
 	def write_dummy(self, output_stream):
 		"""
@@ -90,7 +135,7 @@ class DataType3(DefaultLogging):
 		output_stream.write_byte(3)
 		output_stream.write_int32_unassigned(0)
 
-	def write(self, output_stream):
+	def write(self, output_stream, relative_path):
 		"""
 		write values
 
@@ -100,12 +145,19 @@ class DataType3(DefaultLogging):
 		self._logger.debug("Writing")
 		output_stream.write_byte(3)
 		output_stream.write_int32_unassigned(len(self._docked_entity))
-		for name in self._docked_entity.keys():
-			self._write_dock_entry(output_stream, name, self._docked_entity[name])
+		for dock_index in self._docked_entity.keys():
+			self._docked_entity[dock_index].write(output_stream, dock_index, relative_path)
 
 	# #######################################
 	# ###  Else
 	# #######################################
+
+	def popitem(self):
+		"""
+
+		@rtype: Tuple[Dock_index, DockedEntity]
+		"""
+		return self._docked_entity.popitem()
 
 	def has_data(self):
 		"""
@@ -124,9 +176,7 @@ class DataType3(DefaultLogging):
 		"""
 		output_stream.write("DataType3: {}\n".format(len(self._docked_entity)))
 		if self._debug:
-			for name in self._docked_entity.keys():
-				data = self._docked_entity[name]
-				output_stream.write("{}:\t".format(name))
-				output_stream.write("Position: {}\tSize: {}\t Style {}\t Orientation: {}\n".format(
-					data["position"], data["size"], data["style"], data["orientation"]))
+			for dock_index in self._docked_entity.keys():
+				output_stream.write("{}:\t".format(dock_index))
+				self._docked_entity[dock_index].to_stream(output_stream)
 		output_stream.write("\n")
