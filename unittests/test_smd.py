2dd03d8084b82d93c060f679cae094e99a44716c
p-hofmann <peter.hofmann@hhu.de>
2017-01-24 22:05:09 +0100

    initial commit

---
+++ unittests/test_smd.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+__author__ = 'Peter Hofmann'
+
+
+class TestSmd(TestCase):
+  def test_read(self):
+    self.fail()
+
+  def test_get_block_at_position(self):
+    self.fail()
+
+  def test_get_region_position_of_position(self):
+    self.fail()
+
+  def test_move_center(self):
+    self.fail()
+
+  def test_mirror(self):
+    self.fail()
+
+  def test_get_number_of_blocks(self):
+    self.fail()
+
+  def test_replace_hull(self):
+    self.fail()
+
+  def test_replace_blocks(self):
+    self.fail()
+
+  def test_update(self):
+    self.fail()
+
+  def test_remove_blocks(self):
+    self.fail()
+
+  def test_remove_block(self):
+    self.fail()
+
+  def test_add(self):
+    self.fail()
+
+  def test_has_block_at_position(self):
+    self.fail()
+
+  def test_set_type(self):
+    self.fail()
+
+  def test_auto_hull_shape(self):
+    self.fail()
