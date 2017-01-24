415a7b8828fabe1b39c2cb12fc65d85555e09aa5
p-hofmann <peter.hofmann@hhu.de>
2017-01-24 22:18:35 +0100

    initial commit

--- unittests/test_smd.py
+++ unittests/test_smd.py
@@ -1,50 +1,74 @@
 from unittest import TestCase
+from StringIO import StringIO
+from lib.bits_and_bytes import BinaryStream
+from lib.smblueprint.smd3.smd import Smd
+from blueprints import Blueprint
 
 __author__ = 'Peter Hofmann'
 
 
-class TestSmd(TestCase):
-  def test_read(self):
-    self.fail()
+class DefaultSetup(TestCase):
+    """
+    @type object: Smd
+    """
 
-  def test_get_block_at_position(self):
-    self.fail()
+    def __init__(self, methodName='runTest'):
+        super(DefaultSetup, self).__init__(methodName)
+        self.object = None
+        self._blueprints = Blueprint()
 
-  def test_get_region_position_of_position(self):
-    self.fail()
+    def setUp(self):
+        self.object = Smd()
 
-  def test_move_center(self):
-    self.fail()
+    def tearDown(self):
+        self.object = None
+        # if os.path.exists(self.directory_output):
+        #     os.rmdir(self.directory_output)
 
-  def test_mirror(self):
-    self.fail()
 
-  def test_get_number_of_blocks(self):
-    self.fail()
+class TestSmd(DefaultSetup):
+    def test_read(self):
+        for directory_blueprint in self._blueprints:
+            self.object.read(directory_blueprint)
 
-  def test_replace_hull(self):
-    self.fail()
+    def test_get_block_at_position(self):
+        self.fail()
 
-  def test_replace_blocks(self):
-    self.fail()
+    def test_get_region_position_of_position(self):
+        self.fail()
 
-  def test_update(self):
-    self.fail()
+    def test_move_center(self):
+        self.fail()
 
-  def test_remove_blocks(self):
-    self.fail()
+    def test_mirror(self):
+        self.fail()
 
-  def test_remove_block(self):
-    self.fail()
+    def test_get_number_of_blocks(self):
+        self.fail()
 
-  def test_add(self):
-    self.fail()
+    def test_replace_hull(self):
+        self.fail()
 
-  def test_has_block_at_position(self):
-    self.fail()
+    def test_replace_blocks(self):
+        self.fail()
 
-  def test_set_type(self):
-    self.fail()
+    def test_update(self):
+        self.fail()
 
-  def test_auto_hull_shape(self):
-    self.fail()
+    def test_remove_blocks(self):
+        self.fail()
+
+    def test_remove_block(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_has_block_at_position(self):
+        self.fail()
+
+    def test_set_type(self):
+        self.fail()
+
+    def test_auto_hull_shape(self):
+        self.fail()
