a7adfb7d6b6f64e7b8634f9676d7babe10f81301
p-hofmann <peter.hofmann@hhu.de>
2017-01-24 21:41:00 +0100

    last test

--- unittests/test_meta.py
+++ unittests/test_meta.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 from StringIO import StringIO
 from lib.smblueprint.meta.meta import Meta
 from lib.bits_and_bytes import BinaryStream
-import os
+from blueprints import Blueprint
+
 
 __author__ = 'Peter Hofmann'
 
@@ -15,25 +16,11 @@ class DefaultSetup(TestCase):
     def __init__(self, methodName='runTest'):
         super(DefaultSetup, self).__init__(methodName)
         self.object = None
-        self._blueprints = set()
-        self._file_name = "meta.smbpm"
-
-    def get_blueprints(self, directory_blueprints):
-        list_of_stuff = os.listdir(directory_blueprints)
-        for name in list_of_stuff:
-            path = os.path.join(directory_blueprints, name)
-            if not os.path.isdir(path):
-                continue
-            file_path = os.path.join(path, self._file_name)
-            if not os.path.exists(file_path):
-                continue
-            self._blueprints.add(path)
+        self._blueprints = Blueprint()
 
     def setUp(self):
         # block_config.from_hard_coded()
         self.object = Meta()
-        directory_blueprints = "/home/hofmann/Downloads/starmade-launcher-linux-x64/StarMade/blueprints"
-        self.get_blueprints(directory_blueprints)
 
     def tearDown(self):
         self.object = None
@@ -55,4 +42,6 @@ class TestMeta(DefaultSetup):
         self.object._write_file(input_stream, "./")
 
     def test_move_center_by_vector(self):
-        self.fail()
+        for directory_blueprint in self._blueprints:
+            self.object.read(directory_blueprint)
+            self.object.move_center_by_vector((2, 5, 6))
