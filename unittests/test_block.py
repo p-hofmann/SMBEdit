5550e77fbaaf742c2ebd65b9bdc96144b751e469
p-hofmann <peter.hofmann@hhu.de>
2017-01-24 08:22:47 +0100

    update tests

--- unittests/test_block.py
+++ unittests/test_block.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from lib.smblueprint.smd3.smdblock.block import Block
+from lib.utils.blockconfig import block_config
 
 __author__ = 'Peter Hofmann'
 
@@ -14,6 +15,7 @@ class DefaultSetup(TestCase):
         self.object = None
 
     def setUp(self):
+        block_config.from_hard_coded()
         self.object = Block()
         # wedge 599 0, 0, 0, 3
         # corner 600
@@ -27,7 +29,7 @@ class DefaultSetup(TestCase):
             bit_19=0,
             bit_23=0,
             bit_22=0,
-            rotations=3
+            rotations=1
         )
 
     def tearDown(self):
@@ -41,7 +43,7 @@ class TestBlock(DefaultSetup):
         self.assertEqual(self.object.get_id(), 599)
 
     def test_get_hit_points(self):
-        self.assertEqual(self.object.get_id(), 100)
+        self.assertEqual(self.object.get_hit_points(), 75)
 
     def test_get_style(self):
         self.assertEqual(self.object.get_style(), 1)
@@ -50,10 +52,11 @@ class TestBlock(DefaultSetup):
         self.assertFalse(self.object.is_active())
 
     def test__get_active_value(self):
-        self.fail()
+        self.assertEqual(self.object._get_active_value(), 0)
 
     def test_get_orientation(self):
-        self.fail()
+        expected_orientation = (0, 0, 0, 3)
+        self.assertTupleEqual(self.object.get_orientation(), expected_orientation)
 
     def test_get_int_24bit(self):
         self.fail()
@@ -65,16 +68,30 @@ class TestBlock(DefaultSetup):
         self.fail()
 
     def test_update(self):
-       self.fail()
-
-    def test_set_id(self):
         self.fail()
+
+    def test_set_id(self): #
+        self.object.set_hit_points(604)
+        self.assertEqual(self.object.get_id(), 604)
+        self.test_get_id()
+        self.test_get_style()
+        self.test_is_active()
+        self.test__get_active_value()
+        self.test_get_orientation()
 
     def test_set_hit_points(self):
-        self.fail()
+        self.object.set_hit_points(50)
+        self.assertEqual(self.object.get_hit_points(), 50)
+        self.test_get_id()
+        self.test_get_style()
+        self.test_is_active()
+        self.test__get_active_value()
+        self.test_get_orientation()
 
     def test_set_active(self):
         self.fail()
 
     def test_mirror(self):
-        self.fail()
+        self.object.mirror(0)
+        expected_orientation = (0, 0, 0, 3)
+        self.assertTupleEqual(self.object.get_orientation().get_orientation(), expected_orientation)
