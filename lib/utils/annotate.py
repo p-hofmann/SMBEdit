81c16176a52d62ee43aaf902fb1e9943a035ccdc
p-hofmann <peter.hofmann@hhu.de>
2017-02-12 09:37:11 +0100

    adapted for new Periphery

--- lib/utils/annotate.py
+++ lib/utils/annotate.py
@@ -1,13 +1,10 @@
-import sys
 from lib.utils.blocklist import BlockList
-from lib.utils.blockconfig import block_config
-from lib.smblueprint.smdblock.blockpool import block_pool
 
 
 __author__ = 'Peter Hofmann'
 
 
-class AutoShape(object):
+class Annotate(object):
     """
     Collection of auto shape stuff
 
@@ -30,27 +27,36 @@ class AutoShape(object):
         while len(tmp) > 0:
             position_index = tmp.pop()
             position = self._block_list.get_position(position_index)
-            if min_position >= position or max_position <= position:
+            index = 0
+            if position[index] < min_position[index]-1 or position[index] > max_position[index]+1:
+                continue
+            index = 1
+            if position[index] < min_position[index]-1 or position[index] > max_position[index]+1:
+                continue
+            index = 2
+            if position[index] < min_position[index]-1 or position[index] > max_position[index]+1:
                 continue
             if self._block_list.has_block_at(position):
                 border.add(position_index)
-            else:
-                marked.add(position_index)
-                position_index = self._block_list.get_index((position[0]-1, position[1], position[2]))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
-                position_index = self._block_list.get_index((position[0], position[1]-1, position[2]))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
-                position_index = self._block_list.get_index((position[0], position[1], position[2]-1))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
-                position_index = self._block_list.get_index((position[0]+1, position[1], position[2]))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
-                position_index = self._block_list.get_index((position[0], position[1]+1, position[2]))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
-                position_index = self._block_list.get_index((position[0], position[1], position[2]+1))
-                if position_index not in marked and position_index not in border:
-                    tmp.add(position_index)
+                continue
+
+            marked.add(position_index)
+            position_index = self._block_list.get_index((position[0]-1, position[1], position[2]))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+            position_index = self._block_list.get_index((position[0], position[1]-1, position[2]))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+            position_index = self._block_list.get_index((position[0], position[1], position[2]-1))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+            position_index = self._block_list.get_index((position[0]+1, position[1], position[2]))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+            position_index = self._block_list.get_index((position[0], position[1]+1, position[2]))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+            position_index = self._block_list.get_index((position[0], position[1], position[2]+1))
+            if position_index not in marked and position_index not in border:
+                tmp.add(position_index)
+        return marked, border
--- lib/utils/autoshape.py
+++ lib/utils/autoshape.py
@@ -1,6 +1,6 @@
-import sys
 from lib.utils.blocklist import BlockList
 from lib.utils.blockconfig import block_config
+from lib.utils.periphery import PeripheryBase
 from lib.smblueprint.smdblock.blockpool import block_pool
 
 
@@ -12,99 +12,17 @@ class AutoShape(object):
     Collection of auto shape stuff
 
     @type _block_list: BlockList
+    @type _periphery: PeripheryBase
     """
 
-    def __init__(self, block_list):
+    def __init__(self, block_list, periphery):
         """
 
         @type block_list: BlockList
+        @type periphery: PeripheryBase
         """
         self._block_list = block_list
-
-    def get_position_periphery_index_9x9(self, position):
-        """
-        Every position in a 3x3x3 periphery is represented by a bit.
-
-        @type position: tuple[int]
-        @rtype: int
-        """
-        periphery_index = 0
-        power = 1
-        for x in range(position[0]-1, position[0]+2):
-            for y in range(position[1]-1, position[1]+2):
-                for z in range(position[2]-1, position[2]+2):
-                    position_tmp = (x, y, z)
-                    if position_tmp == position:
-                        continue
-                    if self._block_list.has_block_at(position_tmp):
-                        periphery_index |= power
-                    power <<= 1
-        # print power
-        return periphery_index
-
-    def get_position_periphery_index(self, position, periphery_range):
-        """
-        Some positions in a 3x3x3 periphery, represented by a bit each.
-
-        @type position: tuple[int]
-        @rtype: int
-        """
-        assert 1 <= periphery_range <= 3
-        periphery_index = 0
-        power = 1
-        range_p = [-1, 0, 1]
-        for x in range_p:
-            for y in range_p:
-                for z in range_p:
-                    if abs(x) + abs(y) + abs(z) > periphery_range:
-                        continue
-                    position_tmp = (position[0] + x, position[1] + y, position[2] + z)
-                    if position_tmp == position:
-                        continue
-                    if self._block_list.has_block_at(position_tmp):
-                        periphery_index |= power
-                    power <<= 1
-        return periphery_index
-
-    def get_position_shape_periphery(self, position, periphery_range):
-        """
-        Return a 3x3x3 periphery description
-
-        Shapes:
-            "cube": 0,
-            "Wedge": 1,
-            "Corner": 2,
-            "Tetra": 3,
-            "Hepta": 4,
-            "1/4": 5,
-            "1/2": 6,
-            "3/4": 7,
-
-        @type position: tuple[int]
-        @type periphery_range: int
-
-        @rtype: tple[int]
-        """
-        assert 1 <= periphery_range <= 3
-        angle_shapes = {block_config.get_shape_id("wedge"), block_config.get_shape_id("tetra")}  # 5, 7,
-        shape_periphery = []
-        range_p = [-1, 0, 1]
-        for x in range_p:
-            for y in range_p:
-                for z in range_p:
-                    if abs(x) + abs(y) + abs(z) > periphery_range:
-                        continue
-                    position_tmp = (position[0] + x, position[1] + y, position[2] + z)
-                    if position_tmp == position:
-                        continue
-                    if self._block_list.has_block_at(position_tmp):
-                        block_tmp = self._block_list[position_tmp]
-                        block_id = block_tmp.get_id()
-                        is_angled_shape = False
-                        if block_config[block_id].shape in angle_shapes:
-                            is_angled_shape = True
-                        shape_periphery.append(is_angled_shape)
-        return tuple(shape_periphery)
+        self._periphery = periphery
 
     def auto_hull_shape_independent(self, auto_wedge, auto_tetra):
         """
@@ -118,20 +36,11 @@ class AutoShape(object):
             block_id = block.get_id()
             if not block_config[block_id].is_hull():
                 continue
-
-            periphery_index = self.get_position_periphery_index(position, 1)
-            shape_id_wedge = block_config.get_shape_id("wedge")
-            shape_id_tetra = block_config.get_shape_id("tetra")
-            if auto_wedge and periphery_index in PeripherySimple.peripheries[shape_id_wedge]:
-                # "wedge"
-                new_shape_id = shape_id_wedge
-            elif auto_tetra and periphery_index in PeripherySimple.peripheries[shape_id_tetra]:
-                # tetra
-                new_shape_id = shape_id_tetra
-            else:
+            orientation_simple = self._periphery.get_orientation_simple(
+                position, shape_wedge=auto_wedge, shape_tetra=auto_tetra)
+            if orientation_simple is None:
                 continue
-
-            axis_rotation, rotations = PeripherySimple.peripheries[new_shape_id][periphery_index]
+            new_shape_id, [axis_rotation, rotations] = orientation_simple
             block_hull_tier, color_id, shape_id = block_config[block_id].get_details()
             new_block_id = block_config.get_block_id_by_details(block_hull_tier, color_id, new_shape_id)
             new_block = block_pool(new_block_id).get_modified_block(
@@ -149,14 +58,10 @@ class AutoShape(object):
             block_id = block.get_id()
             if not block_config[block_id].is_hull():
                 continue
-
-            periphery_index = self.get_position_periphery_index(position, 1)
-            if periphery_index not in PeripherySimple.peripheries[block_shape_id]:
+            orientation_complex = self._periphery.get_orientation_complex(position, block_shape_id)
+            if orientation_complex is None:
                 continue
-            periphery_shape = self.get_position_shape_periphery(position, 1)
-            if periphery_shape not in PeripherySimple.peripheries[block_shape_id][periphery_index]:
-                continue
-            axis_rotation, rotations = PeripherySimple.peripheries[block_shape_id][periphery_index][periphery_shape]
+            axis_rotation, rotations = orientation_complex
             block_hull_type, color, shape_id = block_config[block_id].get_details()
             new_block_id = block_config.get_block_id_by_details(block_hull_type, color, block_shape_id)
             new_block = block_pool(new_block_id).get_modified_block(
@@ -179,212 +84,3 @@ class AutoShape(object):
             self.auto_hull_shape_dependent(shape_id_corner)
         if auto_hepta:
             self.auto_hull_shape_dependent(shape_id_hepta)
-
-    def auto_wedge_debug(self):
-        """
-        Replace hull blocks on edges with wedges.
-        """
-        peripheries = {}
-        for position, block in self._block_list.items():
-            if not block_config[block.get_id()].is_hull():
-                continue
-            # wedge 599
-            # corner 600
-            # hepta 601
-            # tetra 602
-            if block.get_id() != 602:
-                continue
-            periphery_index = self.get_position_periphery_index(position, 1)
-            if periphery_index == 0:
-                continue
-            # hull_type, color, shape_id = BlueprintUtils._get_hull_details(block.get_id())
-            axis_rotation = block.get_axis_rotation()
-            rotations = block.get_rotations()
-            if periphery_index in peripheries:
-                orientation = (axis_rotation, rotations)
-                if peripheries[periphery_index] != orientation:
-                    sys.stderr.write("{}: {}\n".format(
-                        periphery_index, orientation))
-                continue
-            sys.stdout.write("\t\t{}: [{}, {}],\n".format(
-                periphery_index, axis_rotation, rotations))
-            # sys.stdout.write("\t\t{}: [{}, {}],  # {}\n".format(
-            #   periphery_index, shape_id, block.get_int_24bit(), position))
-
-            # int_24 = block.get_int_24bit()
-            # block.update(block_id=599, bit_19=bit_19, bit_22=bit_22, bit_23=bit_23, rotations=rotations)
-            # assert int_24 == block.get_int_24bit()
-            peripheries[periphery_index] = (axis_rotation, rotations)
-
-    def auto_hepta_debug(self):
-        """
-        Replace hull blocks on edges with wedges.
-        """
-        peripheries = {}
-        bad_orientations = 0
-        for position, block in self._block_list.items():
-            if not block_config[block.get_id()].is_hull():
-                continue
-            # wedge 599
-            # corner 600
-            # hepta 601
-            # tetra 602
-            if block.get_id() != 601:
-                continue
-            periphery_index = self.get_position_periphery_index(position, 1)
-            periphery_shape = self.get_position_shape_periphery(position, 1)
-            if periphery_index not in peripheries:
-                peripheries[periphery_index] = {}
-
-            axis_rotation = block.get_axis_rotation()
-            rotations = block.get_rotations()
-            orientation = (axis_rotation, rotations)
-
-            if all(periphery_shape):
-                continue
-            if periphery_shape in peripheries[periphery_index] and peripheries[periphery_index][periphery_shape] != orientation:
-                bad_orientations += 1
-                continue
-            peripheries[periphery_index][periphery_shape] = orientation
-
-        print("bad_orientations", bad_orientations)
-        for periphery_index in peripheries:
-            print("\t\t{}:".format(periphery_index), '{')
-            for periphery_shape in peripheries[periphery_index]:
-                print("\t\t\t{}: {},".format(periphery_shape, peripheries[periphery_index][periphery_shape]))
-            print("\t\t},")
-
-
-class PeripherySimple(object):
-    peripheries = dict()
-    # shapes = ["cube", "wedge", "corner", "tetra", "hepta"]
-    # "cube": 0,
-    # "Wedge": 1,
-    # "Corner": 2,
-    # "Tetra": 3,
-    # "Hepta": 4,
-
-    # wedge
-    peripheries[1] = {
-        # [axis rotation, rotations]
-        43: [0, 0],
-        46: [0, 1],
-        39: [0, 2],
-        15: [0, 3],
-        57: [1, 0],
-        29: [1, 1],
-        53: [1, 2],
-        60: [1, 3],
-        27: [2, 0],
-        23: [2, 1],
-        58: [2, 2],
-        54: [2, 3],
-    }
-
-    # corner
-    peripheries[2] = {
-        # (): [bit_19, bit_22, bit_23, rotations]
-        38: {
-            (True, True, False): (5, 0),
-            (True, False, True): (2, 2),
-            (False, True, True): (0, 2),
-        },
-        7: {
-            (False, True, True): (4, 0),
-            (True, False, True): (0, 1),
-            (True, True, False): (2, 3),
-        },
-        42: {
-            (False, True, True): (0, 3),
-            (True, True, False): (5, 3),
-            (True, False, True): (3, 3),
-        },
-        11: {
-            (False, True, True): (4, 3),
-            (True, True, False): (3, 0),
-            (True, False, True): (0, 0),
-        },
-        52: {
-            (False, True, True): (2, 1),
-            (True, True, False): (5, 1),
-            (True, False, True): (1, 2),
-        },
-        21: {
-            (False, True, True): (4, 1),
-            (True, False, True): (2, 0),
-            (True, True, False): (1, 1),
-        },
-        56: {
-            (False, True, True): (3, 2),
-            (True, False, True): (1, 3),
-            (True, True, False): (5, 2),
-        },
-        25: {
-            (False, True, True): (4, 2),
-            (True, True, False): (1, 0),
-            (True, False, True): (3, 1),
-        },
-    }
-
-    # tetra
-    peripheries[3] = {
-        # [axis rotation, rotations]
-        11: [0, 0],
-        7: [0, 1],
-        38: [0, 2],
-        42: [0, 3],
-        25: [1, 0],
-        21: [1, 1],
-        52: [1, 2],
-        56: [1, 3],
-        }
-
-    # hepta
-    peripheries[4] = {
-        59: {
-            (True, False, False, True, False): (0, 3),
-            (True, True, False, False, False): (1, 3),
-            (False, True, False, False, True): (1, 0),
-            (False, False, False, True, True): (0, 0),
-        },
-        47: {
-            (True, False, True, False, False): (0, 3),
-            (True, False, False, True, False): (0, 2),
-            (False, False, False, True, True): (0, 1),
-            (False, False, True, False, True): (0, 0),
-        },
-        55: {
-            (True, True, False, False, False): (1, 2),
-            (True, False, False, True, False): (0, 2),
-            (False, True, False, False, True): (1, 1),
-            (False, False, False, True, True): (0, 1),
-        },
-        63: {
-            (False, True, True, False, False, True): (1, 0),
-            (True, True, True, False, False, False): (1, 3),
-            (True, False, True, False, True, False): (0, 3),
-            (False, False, False, True, True, True): (0, 1),
-            (True, True, False, True, False, False): (1, 2),
-            (False, True, False, True, False, True): (1, 1),
-            (True, False, False, True, True, False): (0, 2),
-            (False, False, True, False, True, True): (0, 0),
-        },
-        61: {
-            (True, False, True, False, False): (1, 2),
-            (False, False, True, False, True): (1, 1),
-            (True, True, False, False, False): (1, 3),
-            (False, True, False, False, True): (1, 0),
-        },
-        62: {
-            (False, True, False, True, False): (0, 3),
-            (False, False, True, True, False): (0, 2),
-            (True, False, True, False, False): (1, 2),
-            (True, True, False, False, False): (1, 3),
-        },
-        31: {
-            (False, True, False, True, False): (1, 1),
-            (False, False, True, False, True): (0, 0),
-            (False, True, True, False, False): (1, 0),
-            (False, False, False, True, True): (0, 1),
-        },
-    }
