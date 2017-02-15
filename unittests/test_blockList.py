b26cc19b95b15f8c75a10616ec731dc224cec9ea
p-hofmann <peter.hofmann@hhu.de>
2017-02-15 13:51:38 +0100

    added blocklist test

---
+++ unittests/test_blockList.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from lib.utils.blocklist import BlockList
+
+
+__author__ = 'Peter Hofmann'
+
+
+class DefaultSetup(TestCase):
+    """
+    @type object: BlockList
+    """
+
+    def __init__(self, methodName='runTest'):
+        super(DefaultSetup, self).__init__(methodName)
+        # self._blueprint = blueprint_handler
+        self.object = None
+
+    def setUp(self):
+        self.object = BlockList()
+
+    def tearDown(self):
+        self.object = None
+
+
+class TestBlockList(DefaultSetup):
+    def test_get_index_get_position(self):
+        position_range = range(-32768, 32767)
+        for x in position_range:
+            for y in [0]:
+                for z in [0]:
+                    expected_position = (x, y, z)
+                    result = self.object.get_position(self.object.get_index(expected_position))
+                    self.assertTupleEqual(expected_position, result)
+
+    # TODO: more tests
