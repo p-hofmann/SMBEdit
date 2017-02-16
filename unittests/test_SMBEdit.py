549c1e6087eae6318f39ee21d004d955d518ffa0
p-hofmann <peter.hofmann@hhu.de>
2017-02-16 19:52:26 +0100

    more tests

--- unittests/test_SMBEdit.py
+++ unittests/test_SMBEdit.py
@@ -15,6 +15,7 @@ class DefaultSetup(TestCase):
         super(DefaultSetup, self).__init__(methodName)
         self.object = None
         self._blueprints = blueprint_handler
+        self._blueprint_test = ""
 
     def setUp(self):
         block_config.from_hard_coded()
@@ -22,44 +23,291 @@ class DefaultSetup(TestCase):
 
     def tearDown(self):
         self.object = None
-        # if os.path.exists(self.directory_output):
-        #     os.rmdir(self.directory_output)
 
 
 class TestSMBEdit(DefaultSetup):
     def test_run(self):
         for blueprint in self._blueprints:
-            options = Options(path_input=blueprint)
+            options = Options()
+            options.path_input = blueprint
             with SMBEdit(
-              options=options,
-              logfile=None,
-              verbose=False,
-              debug=True) as manipulator:
-              manipulator.run()
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_update(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.update = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_summary(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.summary = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_move_center(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.move_center = "2,-3,5"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_entity_type(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.entity_type = 2
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_entity_class(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.entity_class = 2
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_replace(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.replace = "598:507"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_remove_blocks(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.remove_blocks = "598,599"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_replace_hull_blocks(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.replace_hull_blocks = ":a"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_reset_hull_shape(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.reset_hull_shape = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_x(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "x"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_xr(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "xr"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_y(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "y"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_yr(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "yr"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_z(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "z"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_mirror_axis_zr(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.mirror_axis = "zr"
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_auto_wedge(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.auto_wedge = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_auto_tetra(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.auto_tetra = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_auto_corner(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.auto_corner = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_auto_hepta(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.auto_hepta = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
+
+    def test_auto_shape(self):
+        for blueprint in self._blueprints:
+            options = Options()
+            options.path_input = blueprint
+            options.auto_wedge = True
+            options.auto_corner = True
+            options.auto_tetra = True
+            options.auto_hepta = True
+            with SMBEdit(
+                    options=options,
+                    logfile=None,
+                    verbose=False,
+                    debug=True) as manipulator:
+                manipulator.run()
 
 
 class Options(object):
 
-    def __init__(self, starmade_dir=None, path_input=None):
-        self.starmade_dir = starmade_dir
-        self.path_input = path_input
+    def __init__(self):
+        self.tmp_dir = None
+        self.starmade_dir = None
+        self.path_input = None
         self.path_output = None
+        self.summary = None
+        self.silent = True
+        self.update = False
+        self.turn = None
         self.docked_entities = None
         self.link_salvage = None
-        self.turn = None
-        self.replace_hull_blocks = None
-        self.replace = None
-        self.remove_blocks = None
+
         self.move_center = None
-        self.mirror_axis = None
-        self.mirror_axis = None
-        self.update = None
-        self.auto_wedge = None
-        self.auto_tetra = None
-        self.auto_corner = None
-        self.auto_hepta = None
+
         self.entity_type = None
         self.entity_class = None
-        self.summary = None
-        self.tmp_dir = None
+
+        self.remove_blocks = None
         self.reset_hull_shape = False
+        self.replace_hull_blocks = None
+        self.replace = None
+
+        self.mirror_axis = None
+
+        self.auto_wedge = False
+        self.auto_tetra = False
+        self.auto_corner = False
+        self.auto_hepta = False
