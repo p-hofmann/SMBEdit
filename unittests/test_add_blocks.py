76597c7cae513b9ccf90b6d03f573240f2829f51
Sonny Lion <sonny.lion@orange.fr>
2017-07-22 22:40:16 +0200

    Add multiple block IDs to test add_blocks function

--- unittests/test_add_blocks.py
+++ unittests/test_add_blocks.py
@@ -41,11 +41,27 @@ class DefaultSetup(unittest.TestCase):
         positions.sort()
 
         # add blocks
-        block_id_grey_hull = 598 # Grey Hull
+        block_id_grey_hull = 598 # grey hull
+        block_id_grey_hull_wedge = 599 # grey hull wedge
+        block_id_grey_hull_corner = 600 # grey hull corner
+        block_id_grey_hull_tetra = 602 # grey hull tetra
+        block_id_grey_hull_hepta = 601 # grey hull hepta
+        block_id_grey_hull_slabe_34 = 700 # grey hull 3/4
+        block_id_grey_hull_slabe_12 = 699 # grey hull 1/2
+        block_id_grey_hull_slabe_14 = 698 # grey hull 1/4
         self.bp.add_blocks(block_id_grey_hull, positions)
+        self.bp.add_blocks(block_id_grey_hull_wedge, [(0, 0, 0)])
+        self.bp.add_blocks(block_id_grey_hull_corner, [(0, 0, 1)])
+        self.bp.add_blocks(block_id_grey_hull_tetra, [(0, 0, 2)])
+        self.bp.add_blocks(block_id_grey_hull_hepta, [(0, 0, 3)])
+        self.bp.add_blocks(block_id_grey_hull_slabe_34, [(0, 0, 4)])
+        self.bp.add_blocks(block_id_grey_hull_slabe_12, [(0, 0, 5)])
+        self.bp.add_blocks(block_id_grey_hull_slabe_14, [(0, 0, 6)])
 
         # set the entity to space station
         self.bp.set_entity(2, 0)
+
+        self.bp.write('my_test_bp')
 
         # test the number of blocks added (all positions + ship core)
         self.assertEqual(len(positions), self.bp.smd3.get_number_of_blocks())
@@ -53,6 +69,7 @@ class DefaultSetup(unittest.TestCase):
         # test the block position
         bp_positions = list(x for x in self.bp.smd3.get_block_list())
         bp_positions.sort()
+
         self.assertEqual(positions, bp_positions)
 
         # test writing
