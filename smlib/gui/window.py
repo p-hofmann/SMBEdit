4444a5766c2738cbc8301d55b633afcaa8d612a3
hofmann <priggelpit@web.de>
2017-08-07 00:24:17 +0200

    add cheap status bar progress bar

--- smlib/gui/window.py
+++ smlib/gui/window.py
@@ -27,3 +27,23 @@ class Window(QMainWindow):
         #                  x    y    w    h
         self.setGeometry(150, 150, 550, 500)
         self.status_bar.showMessage('Ready')
+
+    def print_progress_bar(self, iteration, total, prefix='', suffix='', decimals=1, length=20, fill='X'):
+        """
+        Original:
+            https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '+' * (length - filled_length)
+        self.status_bar.showMessage('%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
